d74b799f00d54ca335b7dcdbed48940caa6befde
RubinaKhan <rubinatazak@gmail.com>
2024-07-12 14:58:08 +0530

    initial commit

---
+++ frontend/src/Styling/styles.js
@@ -0,0 +1,23 @@
+const styles = {
+  customContainer: "hidden w-11/12 sm:block",
+  title:
+    "font-[600] text-center text-[27px] pb-[20px] font-Roboto md:text-start",
+  sectionWrapper: "mx-auto w-11/12",
+  productHeading: "font-Roboto text-[#333] text-[25px] font-[600]",
+  discountPrice: "text-[18px] font-bold text-[#333] font-Roboto",
+  originalPrice:
+    "text-[16px] line-through mt-[-4px] pl-3 text-[#d55b45] font-[500]",
+  shopName: "pb-3 pt-3 text-blue-400 text-[15px]",
+  indicator: "absolute left-0 bottom-[-27%] bg-[crimson] h-[3px] w-full",
+  primaryButton:
+    "my-3 flex h-[50px] w-[150px] items-center justify-center rounded-xl bg-black cursor-pointer",
+  cartBtn:
+    "cursor-pointer flex h-[38px] items-center justify-center rounded-[20px] bg-[#f63b60] px-[20px]",
+  cartBtnText: "text-[16px] text-[#fff] font-[600]",
+  inputField: "rounded-[5px] border p-1 w-full",
+  statusActive:
+    "absolute top-0 right-1 h-[10px] w-[10px] rounded-full bg-[#40d132]",
+  flexCenter: "flex items-center",
+};
+
+export default styles;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  mode: "jit",
+  theme: {
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"], // Primary sans-serif font
+      heading: ["Poppins", "sans-serif"], // Font for headings
+    },
+    extend: {
+      screens: {
+        sm: "400px", // Small screens (mobile devices)
+        md: "800px", // Medium screens (tablets)
+        lg: "1050px", // Large screens (small laptops)
+        xl: "1110px", // Extra large screens (large laptops/desktops)
+        "2xl": "1300px", // Double extra large screens (large desktops)
+      },
+    },
+  },
+  plugins: [],
+};
