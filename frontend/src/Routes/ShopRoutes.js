5682d7e65f552059bf86a04c192596527731e115
RubinaKhan <rubinatazak@gmail.com>
2024-07-25 18:09:32 +0530

    added redux actions

---
+++ frontend/src/Routes/ShopRoutes.js
@@ -0,0 +1,29 @@
+import ShopDashboardPage from "../Pages/Shop/ShopDashboardPage";
+import ShopCreateProduct from "../Pages/Shop/ShopCreateProduct";
+import ShopAllProducts from "../Pages/Shop/ShopAllProducts";
+import ShopCreateEvents from "../Pages/Shop/ShopCreateEvents";
+import ShopAllEvents from "../Pages/Shop/ShopAllEvents";
+import ShopAllCoupons from "../Pages/Shop/ShopAllCoupouns";
+import ShopPreviewPage from "../Pages/Shop/ShopPreviewPage";
+import ShopAllOrders from "../Pages/Shop/ShopAllOrders";
+import ShopOrderDetails from "../Pages/Shop/ShopOrderDetails";
+import ShopAllRefunds from "../Pages/Shop/ShopAllRefunds";
+import ShopSettingsPage from "../Pages/Shop/ShopSettingsPage";
+import ShopWithDrawMoneyPage from "../Pages/Shop/ShopWithDrawMoneyPage";
+import ShopInboxPage from "../Pages/Shop/ShopInboxPage";
+
+export {
+  ShopDashboardPage,
+  ShopCreateProduct,
+  ShopAllProducts,
+  ShopCreateEvents,
+  ShopAllEvents,
+  ShopAllCoupons,
+  ShopPreviewPage,
+  ShopAllOrders,
+  ShopOrderDetails,
+  ShopAllRefunds,
+  ShopSettingsPage,
+  ShopWithDrawMoneyPage,
+  ShopInboxPage,
+};
