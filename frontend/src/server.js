7df61961286cd09bfa164c654531559262d1e400
RubinaKhan <rubinatazak@gmail.com>
2024-08-15 20:11:47 +0530

    backend url added to frontend

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,2 +1,2 @@
-export const server = "http://localhost:8000/api/v2";
-export const backend_url = "http://localhost:8000";
+export const server = "https://up-grad-project-qx0p515o1-rubinatazaks-projects.vercel.app/api/v2";
+export const backend_url = "https://up-grad-project-qx0p515o1-rubinatazaks-projects.vercel.app/";
