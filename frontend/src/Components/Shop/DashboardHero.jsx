0f6fcd870dffa7c7a3e3a7f076546b207792975e
RubinaKhan <rubinatazak@gmail.com>
2024-08-16 15:12:17 +0530

    changed shop logout functionality again

--- frontend/src/Components/Shop/DashboardHero.jsx
+++ frontend/src/Components/Shop/DashboardHero.jsx
@@ -31,9 +31,7 @@ const DashboardHero = () => {
       minWidth: 130,
       flex: 0.7,
       cellClassName: (params) =>
-        params.getValue(params.id, "status") === "Delivered"
-          ? "greenColor"
-          : "redColor",
+        params.value === "Delivered" ? "greenColor" : "redColor",
     },
     {
       field: "itemsQty",
@@ -54,7 +52,6 @@ const DashboardHero = () => {
       flex: 1,
       minWidth: 150,
       headerName: "",
-      type: "number",
       sortable: false,
       renderCell: (params) => (
         <Link to={`/dashboard-order/${params.id}`}>
@@ -66,9 +63,7 @@ const DashboardHero = () => {
     },
   ];
 
-  const rows = [];
-
-  orders?.forEach((order) => {
+  const rows = orders?.map((order) => {
     const { totalQuantity, totalPrice } = order.cart
       .filter((cartItem) => cartItem.product.shopId === seller._id)
       .reduce(
@@ -79,89 +74,63 @@ const DashboardHero = () => {
         },
         { totalQuantity: 0, totalPrice: 0 }
       );
-    console.log("totalPrice", totalPrice);
-    rows.push({
+
+    return {
       id: order._id,
       itemsQty: totalQuantity,
-      total: `Rs ${totalPrice}`,
+      total: `Rs ${totalPrice.toFixed(2)}`,
       status: order.status,
-    });
+    };
   });
 
   return (
-    <div className='w-full p-8'>
-      <h3 className='text-[22px] font-Poppins pb-2'>Overview</h3>
-      <div className='w-full block md:flex items-center justify-between'>
-        <div className='w-full mb-4 md:w-[30%] min-h-[20vh] bg-[#fde7e7] shadow rounded px-2 py-5'>
-          <div className='flex items-center'>
-            <AiOutlineMoneyCollect
-              size={30}
-              className='mr-2'
-              fill='#00000085'
-            />
-            <h3 className='font-Roboto text-[#333] text-[18px] leading-5 font-semibold text-[#00000085]'>
+    <div className="w-full p-8">
+      <h3 className="text-[22px] font-Poppins pb-2">Overview</h3>
+      <div className="w-full block md:flex items-center justify-between">
+        <div className="w-full mb-4 md:w-[30%] min-h-[20vh] bg-[#fde7e7] shadow rounded px-2 py-5">
+          <div className="flex items-center">
+            <AiOutlineMoneyCollect size={30} className="mr-2" fill="#00000085" />
+            <h3 className="font-Roboto text-[#333] text-[18px] leading-5 font-semibold text-[#00000085]">
               Account Balance{" "}
-              <span className='text-[16px]'>(with 10% service charge)</span>
+              <span className="text-[16px]">(with 10% service charge)</span>
             </h3>
           </div>
-          <h5 className='pt-2 pl-[36px] text-[22px] font-[500]'>
-            Rs {availableBalance}
-          </h5>
-          <Link to='/dashboard-withdraw-money'>
-            <h5 className='pt-4 pl-[2] text-[#d93788] font-semibold'>
-              Withdraw Money
-            </h5>
+          <h5 className="pt-2 pl-[36px] text-[22px] font-[500]">Rs {availableBalance}</h5>
+          <Link to="/dashboard-withdraw-money">
+            <h5 className="pt-4 pl-[2] text-[#d93788] font-semibold">Withdraw Money</h5>
           </Link>
         </div>
 
-        <div className='w-full mb-4 md:w-[30%] min-h-[20vh] bg-[#fde7e7] shadow rounded px-2 py-5'>
-          <div className='flex items-center'>
-            <MdBorderClear size={30} className='mr-2' fill='#00000085' />
-            <h3 className='font-Roboto text-[#333] text-[18px] leading-5 font-semibold text-[#00000085]'>
+        <div className="w-full mb-4 md:w-[30%] min-h-[20vh] bg-[#fde7e7] shadow rounded px-2 py-5">
+          <div className="flex items-center">
+            <MdBorderClear size={30} className="mr-2" fill="#00000085" />
+            <h3 className="font-Roboto text-[#333] text-[18px] leading-5 font-semibold text-[#00000085]">
               All Orders
             </h3>
           </div>
-          <h5 className='pt-2 pl-[36px] text-[22px] font-[500]'>
-            {orders?.length}
-          </h5>
-          <Link to='/dashboard-orders'>
-            <h5 className='pt-4 pl-2 text-[#d93788] font-semibold'>
-              View Orders
-            </h5>
+          <h5 className="pt-2 pl-[36px] text-[22px] font-[500]">{orders?.length}</h5>
+          <Link to="/dashboard-orders">
+            <h5 className="pt-4 pl-2 text-[#d93788] font-semibold">View Orders</h5>
           </Link>
         </div>
 
-        <div className='w-full mb-4 md:w-[30%] min-h-[20vh] bg-[#fde7e7] shadow rounded px-2 py-5'>
-          <div className='flex items-center'>
-            <AiOutlineMoneyCollect
-              size={30}
-              className='mr-2'
-              fill='#00000085'
-            />
-            <h3 className='font-Roboto text-[#333] text-[18px] leading-5 font-semibold text-[#00000085]'>
+        <div className="w-full mb-4 md:w-[30%] min-h-[20vh] bg-[#fde7e7] shadow rounded px-2 py-5">
+          <div className="flex items-center">
+            <AiOutlineMoneyCollect size={30} className="mr-2" fill="#00000085" />
+            <h3 className="font-Roboto text-[#333] text-[18px] leading-5 font-semibold text-[#00000085]">
               All Products
             </h3>
           </div>
-          <h5 className='pt-2 pl-[36px] text-[22px] font-[500]'>
-            {products?.length}
-          </h5>
-          <Link to='/dashboard-products'>
-            <h5 className='pt-4 pl-2 text-[#d93788] font-semibold'>
-              View Products
-            </h5>
+          <h5 className="pt-2 pl-[36px] text-[22px] font-[500]">{products?.length}</h5>
+          <Link to="/dashboard-products">
+            <h5 className="pt-4 pl-2 text-[#d93788] font-semibold">View Products</h5>
           </Link>
         </div>
       </div>
-      <br />
-      <h3 className='text-[22px] font-Poppins pb-2'>Latest Orders</h3>
-      <div className='w-full min-h-[45vh] bg-[#fde7e7] rounded'>
-        <DataGrid
-          rows={rows}
-          columns={columns}
-          pageSize={10}
-          disableSelectionOnClick
-          autoHeight
-        />
+
+      <h3 className="text-[22px] font-Poppins pb-2">Latest Orders</h3>
+      <div className="w-full min-h-[45vh] bg-[#fde7e7] rounded">
+        <DataGrid rows={rows} columns={columns} pageSize={10} disableSelectionOnClick autoHeight />
       </div>
     </div>
   );
