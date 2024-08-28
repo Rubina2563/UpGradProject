414ff7d2c279b0a8b16c1821c1919a973408f91f
RubinaKhan <rubinatazak@gmail.com>
2024-08-28 15:20:22 +0530

    added json limit

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ app.use(
 );
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use("/", express.static(path.join(__dirname,"./uploads")));
 app.use("/test", (req, res) => {
